1400abd38e514b8d7dbb6eb0e1b616e0b9b849a7
debugeverything-project <95109898+debugeverything-project@users.noreply.github.com>
2023-09-17 16:41:56 +1000

    AGAIN

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>!!!!!JS YES!!!!!</h1>`;
+appDiv.innerHTML = `<h1>!!!!!JS YES, AGAIN!!!!!</h1>`;
